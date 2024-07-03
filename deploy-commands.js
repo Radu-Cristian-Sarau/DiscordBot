bb224733da89b4918d452dfd0cf2b3c5f34a6561
Radu-Cristian-Sarau <radusarau2004@outlook.com>
2024-07-03 13:46:33 +0300

    console.log fixes
    fixed console.log messages

--- deploy-commands.js
+++ deploy-commands.js
@@ -19,7 +19,7 @@ for (const folder of commandFolders) {
         if ('data' in command && 'execute' in command) {
             commands.push(command.data.toJSON());           
         } else {
-            console.log('[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.');
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
     }
 }
@@ -30,7 +30,7 @@ const rest = new REST().setToken(token);
 // and deploy your commands!
 (async () => {
     try {
-        console.log('Started refreshing ${commands.length} application (/) commands.');
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
         // The put method is used to fully refresh all commands in the guild with the current set
         const data = await rest.put(
@@ -38,7 +38,7 @@ const rest = new REST().setToken(token);
             {body: commands},
         );
 
-        console.log('Successfully reloaded ${data.length} application (/) commands.');
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     } catch (error) {
         // And of course, make sure you catch and log any errors!
         console.error(error);
